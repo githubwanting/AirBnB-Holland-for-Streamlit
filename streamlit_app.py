831a4389894a91b34e6d3a4a7c66ad552538a6df
githubwanting <36646306+githubwanting@users.noreply.github.com>
2022-12-10 22:08:13 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ dataframe["Airbnb Listing ID"] = dataframe["Airbnb Listing ID"].astype(int)
 # Round of values
 dataframe["Price"] = "€ " + dataframe["Price"].round(2).astype(str) # <--- changes to correct Euro currency
 # Round of values
-dataframe["Meters from chosen loaction"] = dataframe["Meters from chosen location"].astype(int) # <--- meter distance as int so no decimals
+dataframe["Meters from chosen location"] = dataframe["Meters from chosen location"].astype(int) # <--- meter distance as int so no decimals
 
 
 # Rename the number to a string
